e98bce5ba17fc995bd798dad77a0c0fd8738ec50
RizPDK <rizwan.pdk@gmail.com>
2026-09-16 21:47:41 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,5 +37,4 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
-    implementation("com.github.mobile-dev-inc.dadb:dadb:1.2.6")
 }
